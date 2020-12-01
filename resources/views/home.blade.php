ec00ae7b6b0a06e66d16ef281f3e96e538c9c170
ezequiel <ezequielcinalli96@gmail.com>
2020-12-01 19:23:07 -0300

    Added auth and upgrade home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row position-relative">
+        <div class="position-absolute p-3 m-3 bg-white opacity">
+            <h1 class="display-4">Cursando-online</h1>
+            <p class="lead">El mejor sitio de cursos online.</p>
+            <hr class="my-4">
+            <p>El mejor sitio para aprender</p>
+            <a class="btn btn-primary btn-lg" href="{{route('courses')}}" role="button">Ver todos los cursos</a>
+        </div>
+        <img src="images/system/fondo-bienvenida.jpg" class="img-fluid" alt="fondo">
+    </div>
+</div>
+@endsection
